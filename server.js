f177eeadcff8b35a0ffa0f74af9431dda54d7802
Ivon <ivonne@jjf.com>
2021-11-01 10:55:32 -0500

    get working

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const {notesarray } = require('./db/db.json');
+const notesarray = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/api/notes', (req, res) => {
+function findById(id, nArray) {
+    const result = nArray.filter(note => note.id === id)[0];
+    return result;
+}
+
+  app.get('/api/notes', (req, res) => {
   res.json(notesarray);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notesarray);
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
 });
 
 app.listen(PORT, () => {
