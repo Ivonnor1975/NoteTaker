faac931c7a2b6c8b8650e1dd952940cd91252a47
Ivon <ivonne@jjf.com>
2021-11-01 12:50:51 -0500

    add routes for index and notes html files

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+//Express.js middleware that instructs the server to make certain files available
+app.use(express.static('public'));
 
 function findById(id, nArray) {
     const result = nArray.filter(note => note.id === id)[0];
@@ -39,8 +41,24 @@ app.post('/api/notes', (req, res) => {
         path.join(__dirname, './db/db.json'),
         JSON.stringify(notesarray, null, 2))
         }
+    else{
+        res.status(400).send('The note is empty.');
+    }
 });
-  
+
+//load index.html file on browser
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+//load notes.html path to become availablr for browser
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+//default path when no url route matches
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
