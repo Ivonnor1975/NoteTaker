9ddd2e0302e995bb0b87164c27f397a9e505c359
Ivon <ivonne@jjf.com>
2021-11-01 15:03:00 -0500

    use filter to remove notes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,7 @@
 
 const fs = require("fs");
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 //we cannot use app any longer, because it's defined in the server.js 
 //Router, which allows you to declare routes in any file as long as you use the proper middleware.
 const router = require('express').Router();
@@ -14,24 +15,19 @@ router.get('/notes', (req, res) => {
 
 // to create a route for a single animal object
 router.delete('/notes/:id', (req, res) => {
-  console.log(req.params.id);
   let newNotes = [];
-  for(let i = 0; i < notesarray.length; i++) {
-      if(notesarray[i].id !== req.params.id) {
-          newNotes.push(notesarray[i]);
-      }
-  }
+  newNotes= notesarray.filter((note => note.id != req.params.id))
   notesarray = newNotes;
   fs.writeFileSync(
     path.join(__dirname, '../../db/db.json'),
     JSON.stringify(notesarray, null, 2))
 });
 
-  //to create a route to Post or accept data to save on server
-  router.post('/notes', (req, res) => {
+//to create a route to Post or accept data to save on server
+router.post('/notes', (req, res) => {
    // set id based on what the next index of the array will be
    if(req.body) {
-        req.body.id = notesarray.length.toString();
+        req.body.id = uuidv4();  //generate unique id
         notesarray.push(req.body);
         fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
